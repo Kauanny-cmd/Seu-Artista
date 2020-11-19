8459051dc9a524c82f3d59758163417e8254a88a
kauanny <kauannyvieira8@gmail.com>
2020-11-18 23:19:48 -0300

    base de login feito

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -2,11 +2,38 @@ import './styles.css';
 
 function Login() {
     return (
-        <div>
-            <h1>Login</h1>
-            <h2>off</h2>
+    <div id="setinha">
+           
+      
+        <div id="box" >
+           
+            <div>
+                <form action="" method="POST">
+                    <div  id="login-email">
+                        <input name="email" type="email" placeholder="Digite seu email..." id="input-email">
+                        </input>
+                    <div id="login-senha">
+                        <input name="senha" type="password" placeholder="Digite sua senha..." id="input-senha">
+                    </input>
+                    <div id="botao">
+                        <p id="entrar">Entrar</p>
+                    </div>
+                    <div id="botao" >
+                        <button type="submit">
+                            <a href="/" id="entrar">
+                                Entrar
+                            </a>
+                        </button>
+                    </div>
+                    <p id="sem-conta">
+                        <a href="/signup">Não tenho conta</a>
+                    </p>
+                    </div>
+                    </div>
+                </form> 
+            </div>
         </div>
-        
+    </div>
     );
 }
 
